740256b649c2341df8771675cfba82f3429e1b6a
mamenghua <1450125019@qq.com>
2019-10-10 11:26:31 +0800

    mingxieku

--- dist/js/index.js
+++ dist/js/index.js
@@ -133,4 +133,21 @@ $(function () {
 	$(".rBarGoTop").on("click", function () {
 		$("body,html").animate({ "scrollTop": 0 }, 500);
 	});
+
+	$.get("http://47.104.244.134:8080/cartlist.do", {
+		"token": token
+	}, function (data) {
+		var cartNum = 0;
+		for (var i in data) {
+			cartNum += data[i].count;
+		}
+		if (cartNum > 999) {
+			$(".cart-num").text("999+");
+		} else {
+			$(".cart-num").text(cartNum);
+		}
+	});
+
+	$(".navLogin").html("<span style='color:#d30000;font-weight:bold;'>" + token + "</span>");
+	$(".navRegister").html("<a href='html/login.html'>退出</a>");
 });
